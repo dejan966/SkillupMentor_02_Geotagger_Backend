b3f9b69ab66c8932acd13a363265e4a7fcc65ca1
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-03-16 15:41:21 +0100

    Created CRUD for users

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,34 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ClassSerializerInterceptor, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.usersService.findAll();
   }
 
-/*   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
-  } */
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.usersService.findById(id);
+  }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,9 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import Logging from 'src/library/Logging';
 import { Repository } from 'typeorm';
+import { compareHash, hash } from '../utils/bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -28,22 +29,57 @@ export class UsersService {
   }
 
   async findAll() {
-    return await this.usersRepository.find({relations:['quote.user', 'vote.user'], });
+    return await this.usersRepository.find();
   }
 
-  async findBy(condition): Promise<User> {
+  async findBy(condition) {
     return this.usersRepository.findOne({ where: condition });
   }
 
   async findById(id: number) {
-    const user = await this.usersRepository.findOne({ where: { id }, relations: ['quotes.votes.user', 'votes.quote']}) as User;
+    const user = await this.usersRepository.findOne({ where: { id }}) as User;
     return user;
   }
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findById(id);
+    try {
+      for (const key in user) {
+        if (updateUserDto[key]) user[key] = updateUserDto[key];
+      }
+      return this.usersRepository.save(user);
+    } catch (error) {
+      Logging.log(error)
+      throw new NotFoundException('Something went wrong while updating the data.');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async updatePassword(user: User, updateUserDto: {current_password: string, password:string, confirm_password:string}): Promise<User> {
+    if (updateUserDto.password && updateUserDto.confirm_password) {
+      if(!await compareHash(updateUserDto.current_password, user.password)) throw new BadRequestException('Incorrect current password')
+      if (updateUserDto.password !== updateUserDto.confirm_password) throw new BadRequestException('Passwords do not match.')
+      if (await compareHash(updateUserDto.password, user.password)) throw new BadRequestException('New password cannot be the same as old password.')
+      user.password = await hash(updateUserDto.password);
+    }
+    return this.usersRepository.save(user);
+  }
+
+  async updateUserImageId(id:number, avatar:string): Promise<User> {
+    const user = await this.findById(id)
+    if (avatar === user.avatar) {
+      throw new BadRequestException('Avatars have to be different.');
+    }
+    user.avatar = avatar;
+    return this.usersRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findById(id)
+    try {
+      return this.usersRepository.remove(user);
+    } catch (error) {
+      Logging.error(error);
+      throw new InternalServerErrorException('Something went wrong while deleting the account');
+    }
   }
 }
