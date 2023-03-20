f4f326f2dbafe2f43789a361347389f8d43e7bd4
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-03-20 14:47:03 +0100

    Dto for loogs created

---
+++ src/modules/logActions/dto/create-log.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsOptional } from "class-validator";
+import { Action } from "src/entities/action.entity";
+import { Component } from "src/entities/component.entity";
+import { User } from "src/entities/user.entity";
+
+export class CreateLogDto {
+    @IsOptional()
+    action?:Action;
+
+    @IsNotEmpty()
+    component:Component
+
+    @IsNotEmpty()
+    user:User
+}
