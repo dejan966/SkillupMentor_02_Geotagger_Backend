a0e2b439c227d278955d6dda6d418c3c93858365
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-03-20 15:05:57 +0100

    Dto done

--- src/modules/actions/dto/create-action.dto.ts
+++ src/modules/actions/dto/create-action.dto.ts
@@ -1,1 +1,12 @@
-export class CreateActionDto {}
+import { IsNotEmpty, IsOptional } from "class-validator";
+
+export class CreateActionDto {
+    @IsOptional()
+    action: string;
+    
+    @IsNotEmpty()
+    url: string;
+  
+    @IsOptional()
+    new_value: string;
+}
